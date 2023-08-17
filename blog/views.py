e430d41b2b99b78cc2ef0e5c6de742b2466fe24f
julian pulecio <developer@julianpulecio.com>
2023-08-16 21:12:25 -0500

    feature: post comment Model is added, post_comment view is added, and comment template is added

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from django.core.mail import send_mail
-from .models import Post
-from .forms import EmailPostForm
+from django.views.decorators.http import require_POST
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
 
 
 class PostListView(ListView):
@@ -39,6 +40,25 @@ def post_detail(request,year,month,day,post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
+    
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
     return render(request,
                   'blog/post/detail.html',
-                  {'post':post})
+                  {'post':post,
+                   'comments':comments,
+                   'form':form})
+
+@require_POST  
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(request, 'blog/post/comment.html',
+                  {'post':post,
+                   'form':form,
+                   'comment':comment})
