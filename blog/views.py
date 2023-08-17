987e28a5c73f85cfbdb0dccf9c5de4f2a9f363e0
julian pulecio <developer@julianpulecio.com>
2023-08-16 19:10:29 -0500

    feature: post_share view and forms.py ile are added to the Post application to implement the share post by email functionality

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,27 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 from .models import Post
-from django.core.paginator import Paginator
+from .forms import EmailPostForm
 
 
-def post_list(request):
-    posts = Post.published.all()
-    paginator = Paginator(post_list,3)
-    page_number = request.GET.get('page',1)
-    posts = paginator.page(page_number)
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+    else:
+        form = EmailPostForm()
     return render(request,
-                  'blog/post/list.html',
-                  {'posts':posts})
+                  'blog/post/share.html',
+                  {'post':post,
+                   'form':form})
 
 def post_detail(request,year,month,day,post):
     post = get_object_or_404(Post,
