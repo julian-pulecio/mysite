0b72f4bc7d8e6afcfcf512e9768c19fec7ada296
julian pulecio <developer@julianpulecio.com>
2023-08-16 19:46:44 -0500

    feature: the send email functionality is added to the share_post views with the share.html template

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
+from django.core.mail import send_mail
 from .models import Post
 from .forms import EmailPostForm
 
@@ -12,16 +13,24 @@ class PostListView(ListView):
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
     if request.method == 'POST':
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n {cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'julianpuleciogomez@gmail.com',[cd['to']])
+            sent = True
     else:
         form = EmailPostForm()
     return render(request,
                   'blog/post/share.html',
                   {'post':post,
-                   'form':form})
+                   'form':form,
+                   'sent': sent})
 
 def post_detail(request,year,month,day,post):
     post = get_object_or_404(Post,
